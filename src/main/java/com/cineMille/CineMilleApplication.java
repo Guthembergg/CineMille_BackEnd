595d03194f20daa582df2b6911e9b79a9c4f1eff
Guthembergg <karimguettech@gmail.com>
2023-06-15 12:57:19 +0200

    -

---
+++ src/main/java/com/cineMille/CineMilleApplication.java
@@ -0,0 +1,13 @@
+package com.cineMille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineMille/CineMilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineMille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
